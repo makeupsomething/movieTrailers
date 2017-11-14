747f6dd432468bda055547dc02e3a11612be2067
daryl <darylcoleoffice@gmail.com>
2017-11-14 18:02:45 +0900

    improved comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,17 +1,20 @@
 import media
 import fresh_tomatoes
 
+# Toy Story Movie: movie title, storyline, poster image and youtube trailer
 toy_story = media.Movie("Toy Story",
                         "About toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # noqa
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
+# Star Wars Episode IV Movie: movie title, storyline, poster image and youtube trailer
 star_wars = media.Movie("Star Wars",
                         "Awesome space adventures",
                         "https://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg",  # noqa
                         "https://www.youtube.com/watch?v=1g3_CFmnU7k")
 
-muppets_christmas = media.Movie("Muppets Christmas Carole",
+# Muppets Christmas Carol Movie: movie title, storyline, poster image and youtube trailer
+muppets_christmas = media.Movie("Muppets Christmas Carol",
                                 "A christmas classic",
                                 "https://upload.wikimedia.org/wikipedia/en/9/95/Muppet_christmas_carol.jpg",  # noqa
                                 "https://www.youtube.com/watch?v=dhpu2tq9GG4")
--- media.py
+++ media.py
@@ -1,22 +1,28 @@
 import webbrowser
 
-"""
-A Class that creates a movie object.
-Must be initialized with the movie title, storyline, image_url and trailer url, which are all strings.
-"""
-
 
 class Movie:
+
+    """This class creates a movie object"""
+
     def __init__(self,
                  movie_title,
                  movie_storyline,
                  poster_image,
                  trailer_youtube
                  ):
+        """
+        Must be initialized with the following
+        movie title - the title of the movie
+        storyline - a brief description of the plot
+        image_url - a url to the movie's poster
+        trailer url - a link to the trailer on youtube
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """When this function is called the default web browser will open and direct to the youtube_trailer_url"""
         webbrowser.open(self.trailer_youtube_url)
